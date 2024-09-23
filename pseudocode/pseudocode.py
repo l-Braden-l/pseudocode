188a99ca331ec850cf91d94a91975e977f85fe78
braden <leachbr787@northwested.org>
2024-09-23 14:16:55 -0400

    Update pseudocode.py

--- pseudocode/pseudocode.py
+++ pseudocode/pseudocode.py
@@ -1,1 +1,7 @@
-area = int(input())
+
+height = int(input('Chose a number to make the height of a triagle: \n'))
+base = int(input('Chose a number to make the base of a triagle: \n'))
+area = base * height / 2
+areas = f'The area of your triangle is: {int(area)}'
+print(areas)
+
