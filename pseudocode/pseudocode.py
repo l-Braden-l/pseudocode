055f21817eab9c5a6bd58ff67d02ce5de21ea64e
braden <leachbr787@northwested.org>
2024-09-25 13:01:10 -0400

    Update pseudocode.py

--- pseudocode/pseudocode.py
+++ pseudocode/pseudocode.py
@@ -2,6 +2,6 @@
 height = float(input('Chose a number to make the height of a triagle: \n'))
 base = float(input('Chose a number to make the base of a triagle: \n'))
 area = base * height / 2
-areas = f'The area of your triangle is: {str(area)}'
-print(areas)
+
+print(f'The area of your triangle is: {str(area)}')
 
